96467fab6fa05970e5f6c11e78a0c1f11224ad7a
Al Poole <netstar@gmail.com>
2017-06-29 09:51:13 +0100

    .

--- main.c
+++ main.c
@@ -2,44 +2,43 @@
  
 int main(void)
 {
-   Web_Cz *web_obj;
+   Web_Cz *w_obj;
    char *tmp;
    int count = 0;
 
-   web_obj = web_cz_new();
+   w_obj = web_cz_new();
 
-   cookie_t *c = web_obj->cookie.new("AL", "Really nice!");
+   cookie_t *c = w_obj->cookie.new("AL", "Really nice!");
    c->path = "/";
    c->expires = 3600;
-   web_obj->cookie.add(c);
+   w_obj->cookie.add(c);
 
-   cookie_t *c2 = web_obj->cookie.new("Ed", "Skateboard!");
-   web_obj->cookie.add(c2);
+   w_obj->cookie.add(w_obj->cookie.new("Ed", "Skateboard!"));
 
-//   web_obj->session.destroy("netstar");
-   web_obj->session.new("netstar", 3600);
+//   w_obj->session.destroy("netstar");
+   w_obj->session.new("netstar", 3600);
 
-   const char *offering = web_obj->cgi.param("offering");
+   const char *offering = w_obj->cgi.param("offering");
 
-   tmp = web_obj->cgi.param("count");
+   tmp = w_obj->cgi.param("count");
    if (tmp)
      count = atoi(tmp);
 
-   web_obj->cookie.remove("Random");
-   int session = web_obj->session.check("netstar");
+   w_obj->cookie.remove("Random");
+   int session = w_obj->session.check("netstar");
 
    /* BEGIN output */
-   web_obj->headers_display("text/plain");
+   w_obj->headers_display("text/plain");
 
    if (offering && count)
      printf("it is %s and %d\n", offering, count);
 
-   cookie_t *test = web_obj->cookie.get("Ed");
+   cookie_t *test = w_obj->cookie.get("Ed");
    if (test)
      {
         printf("Got value %s\n", test->value);
      }
-   test = web_obj->cookie.get("AL");
+   test = w_obj->cookie.get("AL");
    if (test)
      {
         printf("Got value %s\n", test->value);
@@ -47,7 +46,7 @@ int main(void)
 
    printf("session is %s\n", (session ? "OKAY" : "BAD"));
 
-   web_obj->free();
+   w_obj->free();
 
    return 0;
 }
